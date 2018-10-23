a74a13dbc4d7058d46d00e7b7f15bb66189503da
Aurelio Grott <grott.aurelio@gmail.com>
2018-10-22 23:29:03 -0300

    created power ups

---
+++ 2D-Rogue Like/Assets/Scripts/PowerUps/Eye.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour, IInventoryItem {
+
+	public string Name
+	{
+		get
+		{
+			return "Eye";
+		}
+	}
+
+	public Sprite _Image = null;
+	public Sprite Image
+	{
+		get
+		{
+			return _Image;
+		}
+	}
+
+	public void OnPickup()
+	{
+		gameObject.SetActive(false);
+	}
+}
---
+++ 2D-Rogue Like/Assets/Scripts/PowerUps/Strength.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strength : MonoBehaviour, IInventoryItem {
+
+	public string Name
+	{
+		get
+		{
+			return "Strength";
+		}
+	}
+
+	public Sprite _Image = null;
+	public Sprite Image
+	{
+		get
+		{
+			return _Image;
+		}
+	}
+
+	public void OnPickup()
+	{
+		gameObject.SetActive(false);
+	}
+}
