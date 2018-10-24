4e732a150e62b69c1d238b132df3d07dd64759a7
wilkier <vvilkier@hotmail.com>
2018-10-24 01:03:37 -0300

    new laser

--- 2D-Rogue Like/Assets/Scripts/Laser.cs
+++ 2D-Rogue Like/Assets/Scripts/Laser.cs
@@ -4,24 +4,71 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour {
 
-	private LineRenderer lineRenderer;
+	private LineRenderer[] lineRenderers;
+	private float counterToActivate = 0f;
+	private float counterToDisable = 0f;
+	private bool lasersDisabled = false;
 	public Transform LaserHit;
+	public float timeActive;
+	public float timeDisabled;
 
 	// Use this for initialization
 	void Start ()
 	{
-		lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.enabled = true;
-		// lineRenderer.useWorldSpace = true;
+		lineRenderers = GetComponents<LineRenderer>();
+		for (int i = 0; i < lineRenderers.Length; i++)
+			lineRenderers[i].enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+		counterToDisable += Time.deltaTime;
+		if (counterToActivate > timeDisabled)
+		{
+			counterToDisable = 0f;
+			counterToActivate = 0f;
+			lasersDisabled = false;
+		}
+		if (counterToDisable < timeActive)
+			for (int i = 0; i < lineRenderers.Length; i++)
+				renderLaser(lineRenderers[i]);
+		else
+		{
+			if (!lasersDisabled)
+			{
+				for (int i = 0; i < lineRenderers.Length; i++)
+					disableLaser(lineRenderers[i]);
+				lasersDisabled = true;
+			}
+			counterToActivate += Time.deltaTime;
+		}
+	}
+
+	private void renderLaser(LineRenderer lineRenderer)
+	{
+		RaycastHit2D hit;
+		switch (lineRenderer.name)
+		{
+			case "SpawnUp":
+				hit = Physics2D.Raycast(transform.position, transform.up);
+				break;
+			case "SpawnDown":
+				hit = Physics2D.Raycast(transform.position, -transform.up);
+				break;
+			case "SpawnLeft":
+				hit = Physics2D.Raycast(transform.position, -transform.right);
+				break;
+			case "SpawnRight":
+				hit = Physics2D.Raycast(transform.position, transform.right);
+				break;
+			default:
+				hit = Physics2D.Raycast(transform.position, transform.up);
+				break;
+		}
 		Debug.DrawLine(transform.position, hit.point);
 		LaserHit.position = hit.point;
-		
+		lineRenderer.enabled = true;
 		lineRenderer.SetPosition(0, transform.position);
 		lineRenderer.SetPosition(1, LaserHit.position);
 
@@ -32,5 +79,9 @@ public class Laser : MonoBehaviour {
 				hit.collider.gameObject.GetComponent<Player>().LoseFood(10);
 			}
         }
+	}
+	private void disableLaser(LineRenderer lineRenderer)
+	{
+		lineRenderer.enabled = false;
 	}
 }
