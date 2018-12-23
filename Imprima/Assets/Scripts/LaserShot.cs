b60d1914fd937eb154fcc6d7c43e4e4fb14cd80d
Aurelio Grott <grott.aurelio@gmail.com>
2018-12-23 17:39:48 -0200

    fixing shooting bug. need to get the correct tileset list before compiling

--- Imprima/Assets/Scripts/LaserShot.cs
+++ Imprima/Assets/Scripts/LaserShot.cs
@@ -7,12 +7,16 @@ public class LaserShot : MonoBehaviour {
 
     public int damage;
 
+    void Start()
+    {
+        Debug.LogWarning("positção x: " + this.transform.position.x + " posição y: " + this.transform.position.y);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Food" || other.tag == "Soda" || other.tag == "Exit" || other.tag == "Back" || other.tag == "RedTile" || other.tag == "soundBomb") return;
 
         if (other.tag == "Enemy") other.SendMessage("DamageEnemy", damage, SendMessageOptions.DontRequireReceiver);
-
+        Debug.LogWarning(other);
         if (other.tag != "Player") Destroy(gameObject);
         
     }
